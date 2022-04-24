2afb83ebe7fd48be1733ed7de5d6f3fae7c157b8
nayan-pradhan <nayan.pradhan@hotmail.com>
2022-04-24 16:37:25 +0200

    Updating script for review error detection

--- src/review_error_detection.py
+++ src/review_error_detection.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 from doctest import OutputChecker
-from tkinter import E
 import rospy
 import rosbag
 from std_msgs.msg import Bool, String
@@ -13,48 +12,6 @@ PATH_TO_BAG_FILE = rospy.get_param("path_to_rosbag")
 
 class ReviewErrorDetection:
 
-    def check_latest_msgs(self, old_is_mission_active_msg, old_state_machine_event_log_msg, list_cmd_vel_msg):
-        
-        check_return_msg = "Robot running as expected."
-
-        if old_is_mission_active_msg.data == "False":
-            check_return_msg = "MISSION INACTIVE"
-            rospy.loginfo(check_return_msg)
-            return True, check_return_msg
-
-        try:
-            old_state_machine_event_log_msg = old_state_machine_event_log_msg.data
-            outcome_msg_starting_index = old_state_machine_event_log_msg.index('with outcome:')
-        except:
-            return True, check_return_msg
-
-        outcome_msg = old_state_machine_event_log_msg[outcome_msg_starting_index+14:]
-
-        # rospy.logerr(outcome_msg)
-
-        if (outcome_msg == "paused"):
-            check_return_msg = "STATE MACHINE WAS PAUSED, NOW NO MOVEMENT, SOMETHING WRONG"
-            return False, check_return_msg
-
-        if (outcome_msg == "retry"):
-            
-            is_false_positive = False
-
-            if list_cmd_vel_msg[0].linear.x < self.min_linear_x_threshold:
-                
-                for cmd_vel in list_cmd_vel_msg:
-                    if cmd_vel.linear.x > self.min_linear_x_threshold:
-                        is_false_positive = True 
-                        break
-
-            if not is_false_positive:
-                check_return_msg = "STATE MACHINE TRIED RETRY, NOW NO MOVEMENT, SOMETHING WRONG"
-                return False, check_return_msg
-
-        return True, check_return_msg
-
-
-
     def __init__(self):
         rospy.loginfo("Reviewing ROS BAG file: "+NAME_OF_BAG_FILE)
         bag = rosbag.Bag(PATH_TO_BAG_FILE + '/' + NAME_OF_BAG_FILE)
@@ -62,153 +19,68 @@ class ReviewErrorDetection:
         self.wait_seconds_threshold = int(rospy.get_param("wait_seconds_threshold"))
         self.min_linear_x_threshold = float(rospy.get_param("min_linear_x_threshold"))
 
-        latest_is_mission_active_msg = Bool() 
-        latest_state_machine_event_log_msg = String() 
-        latest_cmd_vel_msg = Twist()
 
-        list_is_mission_active_msg = []
-        list_state_machine_event_log_msg = [] 
-        list_cmd_vel_msg = []
+        all_msgs = []
 
         for topic, msg, t in bag.read_messages(topics=['/mission_active', '/state_machine_event_log', '/cmd_vel']):
             if topic == '/mission_active':
-                latest_is_mission_active_msg = msg
-
-                if len(list_is_mission_active_msg) == self.wait_seconds_threshold:
-                    list_is_mission_active_msg.pop(0)
-
-                list_is_mission_active_msg.append(latest_is_mission_active_msg)
-
+                if msg.data == 'False':
+                    rospy.loginfo("MISSION INACTIVE")
+                    continue 
+                else:
+                    pass
+            
             elif topic == '/state_machine_event_log':
-                latest_state_machine_event_log_msg = msg
-
-                if len(list_state_machine_event_log_msg) == self.wait_seconds_threshold:
-                    list_state_machine_event_log_msg.pop(0)
-
-                list_state_machine_event_log_msg.append(latest_state_machine_event_log_msg)
-
-            
-            elif topic == '/cmd_vel':
-                latest_cmd_vel_msg = msg
-
-                if len(list_cmd_vel_msg) == self.wait_seconds_threshold:
-                    list_cmd_vel_msg.pop(0)
-
-                list_cmd_vel_msg.append(latest_cmd_vel_msg)
-
-            else:
-                rospy.loginfo("Message not received!")
-                continue
-            
-            if len(list_is_mission_active_msg) > 0 and len(list_state_machine_event_log_msg) > 0 and len(list_cmd_vel_msg) > 0:
-                is_everything_fine, check_return_msg = self.check_latest_msgs(list_is_mission_active_msg[0], list_state_machine_event_log_msg[0], list_cmd_vel_msg)
-            
-            else:
-                is_everything_fine, check_return_msg = True, "Initializing messages"
-
-            if (not is_everything_fine):
-                rospy.logerr(check_return_msg)
-                # while (latest_cmd_vel_msg.linear.x < min_linear_x_threshold or not rospy.is_shutdown):
-                #     latest_cmd_vel_msg = self.get_latest_cmd_vel_msg()
-                #     # rospy.loginfo("Linear x vel: "+str(latest_cmd_vel_msg.linear.x))
-                #     rospy.logerr(check_return_msg)
-            else:
-                rospy.loginfo(check_return_msg)
-
-        """
-        recheck = False
-
-        counter = 0
-
-
-        for topic, msg, t in bag.read_messages(topics=['/mission_active', '/state_machine_event_log', '/cmd_vel']):
-            
-            check_return_msg = ""
-            check_return_msg = "ROBOT RUNNING AS EXPECTED"
-
-            if topic == '/mission_active':
-                latest_is_mission_active_msg = msg
-
-                if latest_is_mission_active_msg.data == "False":
-                    check_return_msg = "MISSION INACTIVE"
-                    rospy.loginfo(check_return_msg)
-                    continue
-
-            elif topic == '/cmd_vel':
-                latest_cmd_vel_msg = msg
-
-            elif topic == '/state_machine_event_log':
-                latest_state_machine_event_log_msg = msg
-                latest_state_machine_event_log_msg = latest_state_machine_event_log_msg.data
-
                 try:
-                    outcome_msg_starting_index = latest_state_machine_event_log_msg.index('with outcome:')
+                    msg = msg.data
+                    msg_starting_index = msg.index('with outcome:')
                 except:
-                    rospy.loginfo(check_return_msg)
                     continue
 
-                outcome_msg = latest_state_machine_event_log_msg[outcome_msg_starting_index+14:]
+                outcome_msg = msg[msg_starting_index+14:]
 
-                # rospy.logerr(outcome_msg)
-                print(counter)
+                all_msgs.append(outcome_msg)
 
-                if recheck and latest_cmd_vel_msg.linear.x < 0.15:
-                    counter += 1
-                else:
-                    counter = 0
-                    recheck = False
-
-                if counter > 5:
-                    check_return_msg = "STATE MACHINE WAS PAUSED or TRIED RETRY, NOW NO MOVEMENT, SOMETHING WRONG"
-                    rospy.logerr(check_return_msg)
-                    
-
-                if outcome_msg == "paused" or outcome_msg == "retry":
-                    recheck = True
-                else:
-                    recheck = False
-
-
-                # if recheck and counter > 2:
-                #     if latest_cmd_vel_msg.linear.x < 0.15:
-                #         check_return_msg = "STATE MACHINE WAS PAUSED or TRIED RETRY, NOW NO MOVEMENT, SOMETHING WRONG"
-                #         rospy.logerr(check_return_msg)
-                # else: 
-                #     rospy.loginfo(check_return_msg)
-
-                # if (outcome_msg == "paused" or outcome_msg == "retry"):
-                #     recheck = True 
-                #     counter += 1 
-                # else:
-                #     recheck = False 
-                #     counter = 0 
-
-                # if outcome_msg == "paused" or outcome_msg == "retry" or recheck:
-                #     if latest_cmd_vel_msg.linear.x < 0.15:
-                #         if recheck and counter > 9:
-                #             # print(counter)
-                #             check_return_msg = "STATE MACHINE WAS PAUSED or TRIED RETRY, NOW NO MOVEMENT, SOMETHING WRONG"
-                #             rospy.logerr(check_return_msg)
-                #         else:
-                #             rospy.loginfo(check_return_msg)
-                #         recheck = True
-                #         counter+=1
-                #         # continue
-                # else:
-                #     recheck = False
-                #     counter = 0
-                #     rospy.loginfo(check_return_msg)
-                #     continue
+                if outcome_msg == "paused":
+                    rospy.logerr("STATE MACHINE WAS PAUSED, SOMETHING WRONG")
+            
+            elif topic == '/cmd_vel':
+                all_msgs.append(msg.linear.x)
 
             else:
                 rospy.loginfo("Message not received for t: "+str(t))
                 continue
 
-            """
+        # self.check_msgs(all_msgs)
 
         rospy.loginfo("End of BAG file reached!")
         bag.close()
         rospy.loginfo("BAG file closed!")
+
+
+    def check_msgs(self, all_msgs):
+        # print(all_msgs)
+        flag = False 
+        counter = 0
+
+        for msg in all_msgs:
+
+            if type(msg) == float:
+                if flag:
+                    if msg > self.min_linear_x_threshold:
+                        flag = False 
+                        counter = 0
+                    else:
+                        if counter > self.wait_seconds_threshold:
+                            rospy.logerr("STATE MACHINE TRIED RETRY or PAUSED, NOW NO MOVEMENT, SOMETHING WRONG")
+                        counter += 1
+            else:
+                
+                if msg == 'retry' or msg == 'paused':
+                    flag = True
+                else: 
+                    # flag = False 
+                    rospy.loginfo("Robot running as expected.")
 
 
 def main():
@@ -220,7 +92,6 @@ def main():
     rospy.loginfo("Starting node:   "+NAME_OF_NODE)
 
     ReviewErrorDetection()
-
 
 if __name__ == '__main__':
     main()
