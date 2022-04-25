908689564ee1c1c22eeca2aaf116e09d4bf95d19
nayan-pradhan <nayan.pradhan@hotmail.com>
2022-04-26 00:21:09 +0200

    Adding comments and cleaning up

--- src/review_error_detection.py
+++ src/review_error_detection.py
@@ -14,15 +14,19 @@ class ReviewErrorDetection:
 
     def __init__(self):
         rospy.loginfo("Reviewing ROS BAG file: "+NAME_OF_BAG_FILE)
+        ##> get rosbag
         bag = rosbag.Bag(PATH_TO_BAG_FILE + '/' + NAME_OF_BAG_FILE)
 
         latest_cmd_vel_msg = 0.0
         something_wrong = False
         
+        ##> get parameters from launch file
         self.min_linear_x_threshold = float(rospy.get_param("min_linear_x_threshold"))
 
+        ##> loop through msg from topics in bag file
         for topic, msg, t in bag.read_messages(topics=['/mission_active', '/state_machine_event_log', '/cmd_vel']):
             if topic == '/mission_active':
+                ##> if mission is inactive
                 if msg.data == 'False':
                     rospy.loginfo("MISSION INACTIVE")
                     continue 
@@ -30,6 +34,7 @@ class ReviewErrorDetection:
                     pass
             
             elif topic == '/state_machine_event_log':
+                ##> getting outcome msg from state machine event log
                 try:
                     msg = msg.data
                     msg_starting_index = msg.index('with outcome:')
@@ -38,7 +43,9 @@ class ReviewErrorDetection:
 
                 outcome_msg = msg[msg_starting_index+14:]
 
+                ##> if output msg is 'paused'
                 if outcome_msg == "paused":
+                    ##> if linear velocity in x axis of robot is greater than threshold
                     if latest_cmd_vel_msg < self.min_linear_x_threshold:
                         rospy.logerr("STATE MACHINE WAS PAUSED, NOW NO MOVEMENT, SOMETHING WRONG")
                         something_wrong = True 
@@ -59,10 +66,12 @@ class ReviewErrorDetection:
 
 
 def main():
+    ##> global
     global NAME_OF_NODE
     global NAME_OF_BAG_FILE
     global PATH_TO_BAG_FILE
 
+    ##> initialize ros node
     rospy.init_node(NAME_OF_NODE)
     rospy.loginfo("Starting node:   "+NAME_OF_NODE)
 
